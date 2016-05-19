3db208ea4c0a304a2496c50a772adbcc46520227
Ali Salehi <nysalehi@gmail.com>
2016-05-19 15:37:16 +0430

    Improved ball outline

--- Source/drawing/FieldRendererBall.cpp
+++ Source/drawing/FieldRendererBall.cpp
@@ -6,6 +6,6 @@ void FieldRenderer::DrawBalls(const google::protobuf::RepeatedPtrField<SSL_Detec
 	for (auto ball : data)
 	{
 		this->drawList->AddCircleFilled(ImVec2(ball.x(), ball.y()), ballRadius, IMC_ORANGE, 10);
-		this->drawList->AddCircle(ImVec2(ball.x(), ball.y()), ballRadius, IMC_BLACK, 10);
+		this->drawList->AddCircle(ImVec2(ball.x(), ball.y()), ballRadius+10.f, IMC_BLACK, 10);
 	}
 }
