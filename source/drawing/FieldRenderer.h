48b1c854109d3937abf7cf3faad6f41b4c7c3976
Ali Salehi <nysalehi@gmail.com>
2024-04-22 12:13:10 +0200

    General cleanup.

--- source/drawing/FieldRenderer.h
+++ source/drawing/FieldRenderer.h
@@ -1,22 +1,17 @@
-//
-// Created by lordhippo on 5/18/16.
-//
+#pragma once
 
-#ifndef IMMDBG_DRAWBACKEND_H
-#define IMMDBG_DRAWBACKEND_H
-
-
-#include "imgui.h"
 #include "../utility/scaled_imdrawlist.h"
 
-enum TeamColor {
+enum TeamColor
+{
 	Blue = 0,
 	Yellow = 1,
 };
 
-class FieldRenderer {
+class FieldRenderer
+{
 private:
-	ImDrawListScaled* drawList;
+	ImDrawListScaled *drawList;
 	ImVec2 widgetPos;
 	ImVec2 widgetSize;
 	ImVec2 overallFieldSize;
@@ -29,21 +24,16 @@ private:
 
 public:
 	FieldRenderer();
-	void SetDrawList(ImDrawList* const drawList);
+	void SetDrawList(ImDrawList *const drawList);
 	void SetWidgetProperties(const ImVec2 &pos, const ImVec2 &size);
-	void SetFieldSizeLegacy(const Protos::RoboCup2014Legacy::Geometry::SSL_GeometryFieldSize& field);
+	void SetFieldSizeLegacy(const Protos::RoboCup2014Legacy::Geometry::SSL_GeometryFieldSize &field);
 
-	void DrawFieldLegacy(const Protos::RoboCup2014Legacy::Geometry::SSL_GeometryFieldSize& data) const ;
-	void DrawCameraCalibrations(const google::protobuf::RepeatedPtrField<Protos::SSL_GeometryCameraCalibration>& data) const ;
+	void DrawFieldLegacy(const Protos::RoboCup2014Legacy::Geometry::SSL_GeometryFieldSize &data) const;
+	void DrawCameraCalibrations(const google::protobuf::RepeatedPtrField<Protos::SSL_GeometryCameraCalibration> &data) const;
 
-	void DrawBalls(const google::protobuf::RepeatedPtrField<Protos::Immortals::Debug_Circle>& data) const ;
-	void DrawRobots(const google::protobuf::RepeatedPtrField<Protos::Immortals::Debug_Robot>& data) const ;
+	void DrawBalls(const google::protobuf::RepeatedPtrField<Protos::Immortals::Debug_Circle> &data) const;
+	void DrawRobots(const google::protobuf::RepeatedPtrField<Protos::Immortals::Debug_Robot> &data) const;
 
-	void DrawBalls(const google::protobuf::RepeatedPtrField<Protos::SSL_DetectionBall>& data) const ;
-	void DrawRobots(const google::protobuf::RepeatedPtrField<Protos::SSL_DetectionRobot>& data, TeamColor color) const ;
-
-
+	void DrawBalls(const google::protobuf::RepeatedPtrField<Protos::SSL_DetectionBall> &data) const;
+	void DrawRobots(const google::protobuf::RepeatedPtrField<Protos::SSL_DetectionRobot> &data, TeamColor color) const;
 };
-
-
-#endif //IMMDBG_DRAWBACKEND_H
